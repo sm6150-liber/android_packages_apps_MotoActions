bba1e419fc38a9a9278ef601a3d954bffd95ec96
menorziin <williamsilvapppl@gmail.com>
2022-12-27 23:05:41 -0300

    MotoActions: Include packages/resources/devicesettings
    Change-Id: Ia82418d5ab5cf2a81cf930d6d10a195ff2366fe8

--- Android.mk
+++ Android.mk
@@ -19,7 +19,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res
+    $(LOCAL_PATH)/res \
+    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_DEX_PREOPT := true
